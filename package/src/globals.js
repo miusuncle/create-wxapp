7062f5257f00c5adead8ee27e35cc79578df4d29
miusuncle <miusuncle@gmail.com>
2017-04-06 20:48:07 +0800

    update `src/globals.js`: Add some comments

--- package/src/globals.js
+++ package/src/globals.js
@@ -1,3 +1,8 @@
+/**
+ * Declare some globals
+ * Do NOT touch this file just in case
+ */
+
 module.exports = Object.assign(global, {
 	regeneratorRuntime: require('./vendor/regenerator-runtime'),
 	babelHelpers: require('./vendor/babel-helpers'),
